228ddc62fee784439b71e5f964ac916a26a7e64f
KDVHELL <49861580+KDVHELL@users.noreply.github.com>
2019-05-19 19:42:45 +0500

    Add files via upload

---
+++ Codologia/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        bool isKeyDown1 = false;
+        bool isKeyDown = false;
+        if (Input.GetKey(KeyCode.D))
+        {
+            isKeyDown = true;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            isKeyDown1 = true;
+        }
+
+        _animator.SetBool("IsKeyDown1", isKeyDown1);
+        _animator.SetBool("IsKeyDown", isKeyDown);
+    }
+}
